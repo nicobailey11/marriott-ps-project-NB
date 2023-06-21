683184c215971927ad86efe6bb484922da1543cc
Dani <dani.zaghian@gmail.com>
2023-06-20 18:00:43 -0700

    adds dynamic navbar and map fly function

--- script.js
+++ script.js
@@ -1,51 +1,59 @@
 const PLACES = [
   {
+    name: "Algarve, Portugal",
     long: -7.93044,
     lat: 37.019356,
-    description: "Algarve, Portugal",
     img: "assets/images/popular-destinations/algarve.jpg",
+    category: "beach",
   },
   {
+    name: "Atlanta, Georgia",
     long: -84.38633,
     lat: 33.753746,
-    description: "Atlanta, Georgia",
     img: "assets/images/popular-destinations/atlanta.jpg",
+    category: "city",
   },
   {
+    name: "Bali, Indonesia",
     long: 115.188919,
     lat: -8.409518,
-    description: "Bali, Indonesia",
     img: "assets/images/popular-destinations/bali.jpg",
+    category: "beach",
   },
   {
+    name: "Bass Lake, California",
     long: -119.5664,
     lat: 37.3247,
-    description: "Bass Lake, California",
     img: "assets/images/popular-destinations/bass-lake.jpg",
+    category: "mountains",
   },
   {
+    name: "Big Sky, Montana",
     long: -111.25312,
     lat: 45.26599,
-    description: "Big Sky, Montana",
     img: "assets/images/popular-destinations/big-sky.jpg",
+    category: "mountains",
   },
   {
+    name: "Delray Beach, Florida",
     long: -80.105545,
     lat: 26.459763,
-    description: "Delray Beach, Florida",
     img: "assets/images/popular-destinations/delray-beach.jpg",
+    category: "beach",
   },
   {
+    name: "Marco Island, Florida",
     long: -81.714722,
     lat: 25.940556,
-    description: "Marco Island, Florida",
     img: "assets/images/popular-destinations/marco-island.jpg",
+    category: "beach",
   },
   {
+    name: "Nashville, Tennessee",
     long: -86.76796,
     lat: 36.174465,
-    description: "Nashville, Tennessee",
     img: "assets/images/popular-destinations/nashville.jpg",
+    category: "city",
   },
 ];
 
@@ -58,12 +66,39 @@ const map = new mapboxgl.Map({
 });
 
 PLACES.forEach((place) => {
-  //   var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+  // add a dropdown item to the nav menu with goToMap function
+  const megaMenuCol1 = document.getElementById("mega-menu-col-1");
+  const megaMenuCol2 = document.getElementById("mega-menu-col-2");
+  let menuItemContent = `
+  <li onclick="goToMap('${place.name}')">
+    <a class="dropdown-item" href="#">
+      ${place.name}
+    </a>
+  </li>
+  `;
+  if (megaMenuCol1.childElementCount < 4) {
+    megaMenuCol1.insertAdjacentHTML("beforeend", menuItemContent);
+  } else {
+    megaMenuCol2.insertAdjacentHTML("beforeend", menuItemContent);
+  }
+
+  // set a marker with a popup on the map
   var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-    `<p class="text-center">${place.description}</p> <img src="${place.img}" alt="" style="width: 200px; height: auto; border-radius: 4px;">`
+    `<p class="text-center">${place.name}</p> <img src="${place.img}" alt="" style="width: 200px; height: auto; border-radius: 4px;">`
   );
   new mapboxgl.Marker({ color: "black" })
     .setLngLat([place.long, place.lat])
     .setPopup(popup)
     .addTo(map);
 });
+
+function goToMap(placeName) {
+  // find place object by name in PLACES array
+  let placeObj = PLACES.find((place) => place.name === placeName);
+  // TODO: scroll to map on page
+  document.getElementById("map").scrollIntoView();
+
+  map.flyTo({
+    center: [placeObj.long, placeObj.lat],
+  });
+}
